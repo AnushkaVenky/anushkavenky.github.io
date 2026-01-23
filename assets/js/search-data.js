44f1447584baadb49e74d5c993125469ec6c229b
AnushkaVenky <me21b026@smail.iitm.ac.in>
2026-01-23 10:35:31 +0000

    Deploying to gh-pages from @ AnushkaVenky/anushkavenky.github.io@5da1e5a34016237bc06b13008c8d348a6c606b1c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -46,9 +46,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-identifying-new-reaction-pathways-for-reactions-under-vibrationally-excited-conditions",
+          title: 'Identifying new reaction pathways for reactions under vibrationally excited conditions',
+          description: "Master&#39;s Thesis Project",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
