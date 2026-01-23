0a9432f9da3ac41fa70e98c34346934bdb0b6040
AnushkaVenky <me21b026@smail.iitm.ac.in>
2026-01-23 10:44:12 +0000

    Deploying to gh-pages from @ AnushkaVenky/anushkavenky.github.io@e87eb23d207dca333acc20418ee3272d3b68856b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,9 +51,9 @@ ninja.data = [{
           description: "Master&#39;s Thesis Project",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-smiles-to-liquid-properties",
+          title: 'SMILES to Liquid Properties',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
